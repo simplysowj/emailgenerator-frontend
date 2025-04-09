12867bcc8f976abb7fbe6d0aa64913ad52875273
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-04-09 19:07:51 -0400

    Update axios.jsx

--- src/axios.jsx
+++ src/axios.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an Axios instance with default settings
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/', // Make sure this points to your Django API
+  baseURL: 'https://email-backend-bee9bjdec6gkhuf3.eastus2-01.azurewebsites.net/api/', // Make sure this points to your Django API
   headers: {
     'Content-Type': 'application/json',
   },
